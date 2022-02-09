7e7117c06c111313add5a49c337f06bb010620e1
ArdaBakici <arda.bakici@stu.bahcesehir.k12.tr>
2022-02-10 02:22:37 +0300

    print num of classes

--- detect.py
+++ detect.py
@@ -78,7 +78,6 @@ def detect(save_img=False):
         # Apply Classifier
         if classify:
             pred = apply_classifier(pred, modelc, img, im0s)
-
         # Process detections
         for i, det in enumerate(pred):  # detections per image
             if webcam:  # batch_size >= 1
